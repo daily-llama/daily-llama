23a4e554ca8a089a73de631c68a7e2555a67e6b3
SamGerber <gerbyxvi@gmail.com>
2016-02-13 17:58:30 -0800

    Annotate

--- app/helpers/captions_helper.rb
+++ app/helpers/captions_helper.rb
@@ -1,2 +1,14 @@
+# == Schema Information
+#
+# Table name: captions
+#
+#  id         :integer          not null, primary key
+#  text       :string           not null
+#  picture_id :integer          not null
+#  user_id    :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 module CaptionsHelper
 end
