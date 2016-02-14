9c07f348388aadbb4ab3a54b078ad87da900ca80
Godsten Silver <geeausten@gmail.com>
2016-02-13 17:41:42 -0800

    pictureControllers

---
+++ app/controllers/rails_controller.rb
@@ -0,0 +1,10 @@
+class RailsController < ApplicationController
+  def g
+  end
+
+  def controller
+  end
+
+  def Captions
+  end
+end
--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,2 +1,10 @@
 class Picture < ActiveRecord::Base
+  validates :picture_url, presence: true
+
+  has_many :captions
+
+  def caption_order
+    # returns array of captions in the order of the most votes
+  end
+
 end
